e1d858a52c17af41d732316e64a0e448faede0e7
Alexander Kämmerer <atothek@gmail.com>
2022-05-17 13:29:51 +0200

    fix:  fixed FetchProvider options to use provided baseUrl

--- packages/client/src/context/FetchContext/FetchProvider.tsx
+++ packages/client/src/context/FetchContext/FetchProvider.tsx
@@ -10,8 +10,9 @@ interface FetchProviderProps extends React.PropsWithChildren<unknown> {
 
 export function FetchProvider(props: FetchProviderProps) {
     const { children, options } = props;
+
     const baseUrl =
-        (options && options.baseUrl) || window.location.href.endsWith("/")
+        ( options && options.baseUrl ) ? options.baseUrl : window.location.href.endsWith("/")
             ? window.location.href.substring(0, window.location.href.length - 1)
             : window.location.href;
 
