bc08917c040bf54dad10ff1b8cc8721deeb29f51
Alexander Kämmerer <atothek@gmail.com>
2022-05-17 13:30:26 +0200

    fix:  export FetchOptions as type only

--- packages/client/src/context/FetchContext/index.ts
+++ packages/client/src/context/FetchContext/index.ts
@@ -1,3 +1,4 @@
 export { FetchProvider } from "./FetchProvider";
 export { useFetch } from "./useFetch";
 export { useFetchContext } from "./useFetchContext";
+export type { FetchOptions } from "./useFetchContext";
